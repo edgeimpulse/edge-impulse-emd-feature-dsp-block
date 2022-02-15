eb25586b9e649be69d47ffa4ffc05f9852034a6d
Omar Shrit <omar@shrit.me>
2022-02-15 14:10:00 +0000

    Add csv extension
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- dataset_ei_converter/main.cpp
+++ dataset_ei_converter/main.cpp
@@ -7,7 +7,7 @@ int main()
   for (size_t i = 0; i < dataset.n_rows; ++i)
   {  
     arma::rowvec feature_vector =  dataset.row(i);
-    std::string name = "class" + std::to_string(feature_vector.at(feature_vector.n_elem - 1)) + ".__" + std::to_string(i);
+    std::string name = "class" + std::to_string(feature_vector.at(feature_vector.n_elem - 1)) + ".__" + std::to_string(i) + ".csv";
     feature_vector.shed_col(feature_vector.n_elem - 1);
     feature_vector.save(name, arma::csv_ascii);
   }
